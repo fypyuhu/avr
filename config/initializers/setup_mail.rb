5bbdd3f619846e054cb9bf1289e8675d49a1fff1
Muhammad Usama Riaz <muhammadusamariaz@gmail.com>
2016-03-08 13:28:06 -0800

    ss

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.delivery_method = :smtp # be sure to choose SMTP delivery
 ActionMailer::Base.smtp_settings = {
-    :address              => "162.243.207.6",
+    :address              => "localhost",
    # :port                 => 587,
     :domain               => "rails-unicorn-nginx",
     :user_name            => "rails",
